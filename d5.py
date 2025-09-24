c187e98b6e9097aa3c67f6d280b71697f5878740
gonzoc2 <gcortes@esgari.com.mx>
2025-09-24 15:41:25 -0600

    Update d5.py

--- d5.py
+++ d5.py
@@ -542,7 +542,6 @@ else:
             output = BytesIO()
             with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
                 df.to_excel(writer, index=False, sheet_name='Reporte')
-                writer.save()
             processed_data = output.getvalue()
             return processed_data
 
@@ -597,6 +596,7 @@ else:
 
     else:
         st.sidebar.warning("Por favor, sube los archivo para continuar.")
+
 
 
 
